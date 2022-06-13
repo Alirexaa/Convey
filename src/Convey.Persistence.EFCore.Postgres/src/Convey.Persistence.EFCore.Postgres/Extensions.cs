aaeeefda863c47d92cb6db5c54e968c068d9cb65
Alirexaa <Alireza.Baloochi1380@gmail.com>
2022-06-13 19:47:26 +0430

    Postgres Extention

--- src/Convey.Persistence.EFCore.Postgres/src/Convey.Persistence.EFCore.Postgres/Extensions.cs
+++ src/Convey.Persistence.EFCore.Postgres/src/Convey.Persistence.EFCore.Postgres/Extensions.cs
@@ -1,44 +1,55 @@
-﻿//using Convey.Persistence.EFCore.Postgres.Builder;
-//using Microsoft.Extensions.DependencyInjection;
+﻿using Convey.Persistence.EFCore.Postgres.Builder;
+using Convey.Persistence.EFCore.Postgres.Internals;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
-//namespace Convey.Persistence.EFCore.Postgres;
+namespace Convey.Persistence.EFCore.Postgres;
 
-//public static class Extensions
-//{
-//    private const string SectionName = "postgres";
-//    private const string RegistryName = "persistence.postgres";
+public static class Extensions
+{
+    private const string SectionName = "postgres";
+    private const string RegistryName = "persistence.postgres";
 
-//    public static IConveyBuilder AddPostgresDbContext(this IConveyBuilder builder, string sectionName = SectionName)
-//    {
-//        if (string.IsNullOrWhiteSpace(sectionName))
-//        {
-//            sectionName = SectionName;
-//        }
+    public static IConveyBuilder AddPostgresDbContext<TApplicationDbContext>(this IConveyBuilder builder, string sectionName = SectionName) where TApplicationDbContext : DbContext
+    {
+        if (string.IsNullOrWhiteSpace(sectionName))
+        {
+            sectionName = SectionName;
+        }
 
-//        var options = builder.GetOptions<PostgresOptions>(sectionName);
-//        return builder.AddPostgresDbContext(options);
-//    }
+        var options = builder.GetOptions<PostgresOptions>(sectionName);
+        return builder.AddPostgresDbContext<TApplicationDbContext>(options);
+    }
 
-//    public static IConveyBuilder AddPostgresDbContext(this IConveyBuilder builder,
-//        Func<IPostgresOptionsBuilder, IPostgresOptionsBuilder> buildOptions)
-//    {
-//        var options = buildOptions(new PostgresOptionsBuilder()).Build();
-//        return builder.AddPostgresDbContext(options);
-//    }
+    public static IConveyBuilder AddPostgresDbContext<TApplicationDbContext>(this IConveyBuilder builder,
+        Func<IPostgresOptionsBuilder, IPostgresOptionsBuilder> buildOptions) where TApplicationDbContext : DbContext
+    {
+        var options = buildOptions(new PostgresOptionsBuilder()).Build();
+        return builder.AddPostgresDbContext<TApplicationDbContext>(options);
+    }
 
-//    public static IConveyBuilder AddPostgresDbContext(this IConveyBuilder builder, PostgresOptions options)
-//    {
-//        if (!builder.TryRegister(RegistryName))
-//        {
-//            return builder;
-//        }
+    public static IConveyBuilder AddPostgresDbContext<TApplicationDbContext>(this IConveyBuilder builder, PostgresOptions options) where TApplicationDbContext : DbContext
+    {
+        if (!builder.TryRegister(RegistryName))
+        {
+            return builder;
+        }
 
-//        builder.Services
-//            .AddSingleton(options)
-//            .AddDbContext(opt =>
-//            {
-//                opt.UseNpgsql(options.ConnectionString);
-//            });
-//        return builder;
-//    }
-//}
+        builder.Services
+            .AddSingleton(options)
+            .AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWorkEf<>))
+            .AddScoped(typeof(IRepository<>),typeof(Repository<,>))
+            .AddDbContext<TApplicationDbContext>(opt =>
+            {
+                if (options.UseInMemoryDatabase)
+                {
+                    opt.UseInMemoryDatabase(options.InMemoryDatabaseName);
+                }
+                else
+                {
+                    opt.UseNpgsql(options.ConnectionString);
+                }
+            });
+        return builder;
+    }
+}
--- src/Convey.Persistence.EFCore.Postgres/src/Convey.Persistence.EFCore.Postgres/PostgresOptions.cs
+++ src/Convey.Persistence.EFCore.Postgres/src/Convey.Persistence.EFCore.Postgres/PostgresOptions.cs
@@ -3,4 +3,6 @@
 public class PostgresOptions
 {
     public string ConnectionString { get; set; }
+    public string InMemoryDatabaseName { get; set; }
+    public bool UseInMemoryDatabase { get; set; }
 }
